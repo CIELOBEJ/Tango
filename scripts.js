c025d9c2cc0ffed450c7c8e8c776af77b7769d9b
CIELOBEJ <cielobej77@gmail.com>
2018-03-08 17:50:00 +0100

    html, agg jquery

--- scripts.js
+++ scripts.js
@@ -47,3 +47,16 @@ function myFunction() {
         x.className = "navbar";
     }
 }
+
+
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 520) {
+            $(".main-header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".main-header").removeClass("active");
+        }
+    });
+});
