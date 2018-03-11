efe7cf7c40f1bc5048c0d2f67011dd32a6f4d071
CIELOBEJ <cielobej77@gmail.com>
2018-03-11 15:57:45 +0100

    add class active

--- scripts.js
+++ scripts.js
@@ -52,7 +52,7 @@ function myFunction() {
 
 $(function() {
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 520) {
+        if($(window).scrollTop() > 50) {
             $(".main-header").addClass("active");
         } else {
             //remove the background property so it comes transparent again (defined in your css)
